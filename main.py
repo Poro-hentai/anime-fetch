c02e929009ee348e4c3cc8e8c245d3f58490dd3d
Poro-hentai <shadowsama128@gmail.com>
2025-06-21 13:19:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -481,8 +481,8 @@ async def users(update: Update, context: ContextTypes.DEFAULT_TYPE):
 @admin_only
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     files = [POSTS_FILE, USERS_FILE, REQUESTS_FILE]
-    for file in files:
-        if os.path.exists(file):
+    for file in files:
+        if os.path.exists(file):
             await update.message.reply_document(document=open(file, "rb"), filename=file)
         else:
             await update.message.reply_text(f"❌ File `{file}` not found.", parse_mode="Markdown")
